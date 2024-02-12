e91d104eb9525e5c4fe62fea1975ac9a67456a42
karchal <k.s.chalupka@gmail.com>
2024-02-12 13:19:28 +0100

    Initial commit: Setting up project structure

---
+++ src/main/java/com/example/githubexplorer/GithubExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubExplorerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/githubexplorer/GithubExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
